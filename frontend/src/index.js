c11f5100019fcebe43d29eb8149c892feb24b7ee
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-02-26 21:39:10 +0100

    Refactor window functionality
    To help to have the best organized code, make a small
    refactor of the function which paint the windows
    cookies.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
-let right_window = document.getElementsByClassName("window-right")[0];
-let left_window = document.getElementsByClassName("window-left")[0];
+function windowManager() {
+  let right_window = document.getElementsByClassName("window-right")[0];
+  let left_window = document.getElementsByClassName("window-left")[0];
 
-function addCookieWindow(element) {
-  element.addEventListener("mouseover", e => {
-    e.target.style.backgroundImage =
-      "url('https://emojis.wiki/emoji-pics/lg/cookie-lg.png')";
-  });
+  function addCookieWindow(element) {
+    element.addEventListener("mouseover", e => {
+      e.target.style.backgroundImage =
+        "url('https://emojis.wiki/emoji-pics/lg/cookie-lg.png')";
+    });
 
-  element.addEventListener("mouseout", e => {
-    e.target.style.background = "#c7e9ff";
-  });
+    element.addEventListener("mouseout", e => {
+      e.target.style.background = "#c7e9ff";
+    });
+  }
+
+  addCookieWindow(right_window);
+  addCookieWindow(left_window);
 }
 
-addCookieWindow(right_window);
-addCookieWindow(left_window);
+windowManager();
