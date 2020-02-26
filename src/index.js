2184ed21724d285b356e4371ae317685e169a07f
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-02-26 10:15:13 +0100

    Load the image cookie in the window
    If the user passes the mouse over one of the windows, it will
    change and load an image of the cookie, on the contrary
    if the user removes the mouse, the window will have the
    same color as the default one.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+let right_window = document.getElementsByClassName("window-right")[0];
+let left_window = document.getElementsByClassName("window-left")[0];
 
+function addCookieWindow(element) {
+  element.addEventListener("mouseover", e => {
+    e.target.style.backgroundImage =
+      "url('https://emojis.wiki/emoji-pics/lg/cookie-lg.png')";
+  });
+
+  element.addEventListener("mouseout", e => {
+    e.target.style.background = "#c7e9ff";
+  });
+}
+
+addCookieWindow(right_window);
+addCookieWindow(left_window);
